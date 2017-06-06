fdde759b1b70a6329771885024da8ebb2eeea916
rtgdk <rohit.lodhartg@gmail.com>
2017-06-06 23:10:21 +0530

    Added models.py
    Signed-off-by: rtgdk <rohit.lodhartg@gmail.com>

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import os
+from datetime import datetime
+from django import forms
+from django.contrib.auth.models import User
 
+class UserID(models.Model):
+	user = models.OneToOneField(User)
+	password = models.CharField("Password",max_length=32, null=False, blank=False)
+	organisation = models.CharField("Organisation",max_length=64, null=False, blank=False)
+	lastlogin = models.DateField(blank=True)
+	
 # Create your models here.
