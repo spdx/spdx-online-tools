b3b152b4645c276b794502c111b6d0c3df7a9e5f
rtgdk <rohit.lodhartg@gmail.com>
2017-06-13 14:52:17 +0530

    Add basic test for context views HttpResponse
    Signed-off-by: rtgdk <rohit.lodhartg@gmail.com>

--- project/app/tests.py
+++ project/app/tests.py
@@ -4,3 +4,43 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+
+class IndexViewsTestCase(TestCase):
+	def test_index(self):
+		resp = self.client.get('/app/')
+		self.assertEqual(resp.status_code,200)
+	
+class AboutViewsTestCase(TestCase):
+	def test_about(self):
+		resp = self.client.get('/app/about/')
+		self.assertEqual(resp.status_code,200)
+
+class ValidateViewsTestCase(TestCase):
+	def test_validate(self):
+		resp = self.client.get('/app/validate/')
+		self.assertEqual(resp.status_code,200)
+	
+class CompareViewsTestCase(TestCase):
+	def test_compare(self):
+		resp = self.client.get('/app/compare/')
+		self.assertEqual(resp.status_code,200)
+
+class ConvertViewsTestCase(TestCase):
+	def test_convert(self):
+		resp = self.client.get('/app/convert/')
+		self.assertEqual(resp.status_code,200)
+	
+class SearchViewsTestCase(TestCase):
+	def test_search(self):
+		resp = self.client.get('/app/search/')
+		self.assertEqual(resp.status_code,200)
+
+class LoginViewsTestCase(TestCase):
+	def test_login(self):
+		resp = self.client.get('/app/login/')
+		self.assertEqual(resp.status_code,200)
+	
+class RegisterViewsTestCase(TestCase):
+	def test_register(self):
+		resp = self.client.get('/app/register/')
+		self.assertEqual(resp.status_code,200)
