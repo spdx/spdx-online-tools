2e521340cc4e306f4ef4e9c8ed6820ab498b2307
rtgdk <rohit.lodhartg@gmail.com>
2017-06-16 22:26:54 +0530

    Change top level directory from project to src
    Signed-off-by: rtgdk <rohit.lodhartg@gmail.com>

---
+++ src/app/forms.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017 Rohit Lodha
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import unicode_literals
+
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.admin import widgets 
+
+from app.models import UserID
+
+
+class UserRegisterForm(forms.ModelForm):
+	password = forms.CharField(widget=forms.PasswordInput())
+	confirm_password = forms.CharField(widget=forms.PasswordInput())
+	def clean_username(self):
+		users = self.cleaned_data["username"]
+		if User.objects.filter(username=users).count() > 0:
+			raise forms.ValidationError("This username already exists.")
+		return users
+	def clean(self):
+		password = self.cleaned_data.get('password')
+		confirm_password = self.cleaned_data.get('confirm_password')
+		if password and password != confirm_password:
+			raise forms.ValidationError("Passwords don't match.")
+		return self.cleaned_data
+	class Meta:
+		model = User
+		fields = ('first_name','last_name','email','username','password','confirm_password')
+
+class UserProfileForm(forms.ModelForm):
+	class Meta:
+		model = UserID
+		fields = ('organisation',)
+		
---
+++ src/src/urls.py
@@ -0,0 +1,43 @@
+
+# Copyright (c) 2017 Rohit Lodha
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""src URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url,include
+from django.contrib import admin
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'src.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+		url(r'^(/)?$', RedirectView.as_view(url='/app/')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^app/', include('app.urls')),
+]
+
+urlpatterns += staticfiles_urlpatterns()
