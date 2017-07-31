108d2b4c3115d41079eaba60c48496a4dfe79341
rtgdk <rohit.lodhartg@gmail.com>
2017-08-01 01:17:52 +0530

    Add models for api
    Signed-off-by: rtgdk <rohit.lodhartg@gmail.com>

--- src/api/models.py
+++ src/api/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class FileUpload(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, to_field='id')
+    datafile = models.FileField()
