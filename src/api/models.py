0999b8bd80edbb96f6e9903e32152f0e5b9f76ef
rtgdk <rohit.lodhartg@gmail.com>
2017-08-02 15:56:46 +0530

    Add compare api to models
    Signed-off-by: rtgdk <rohit.lodhartg@gmail.com>

--- src/api/models.py
+++ src/api/models.py
@@ -14,3 +14,10 @@ class ConvertFileUpload(models.Model):
     type = models.CharField(max_length=16,null=False,blank=False)
     result = models.CharField(max_length=32,null=False,blank=False)
     file = models.FileField()
+
+class CompareFileUpload(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, to_field='id')
+    result = models.CharField(max_length=32,null=False,blank=False)
+    file1 = models.FileField()
+    file2 = models.FileField()
