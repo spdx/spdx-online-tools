8fae4792419144086c69edadd461bc39be1f1266
rtgdk <rohit.lodhartg@gmail.com>
2017-08-17 02:26:44 +0530

    Add copyright according to pep4 to api files
    Signed-off-by: rtgdk <rohit.lodhartg@gmail.com>

--- src/api/views.py
+++ src/api/views.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2017 Rohit Lodha
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from __future__ import unicode_literals
 
-from rest_framework.parsers import FileUploadParser
+from rest_framework.parsers import FileUploadParser,FormParser, MultiPartParser
 from rest_framework.response import Response
-from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.viewsets import ModelViewSet
 from models import ValidateFileUpload,ConvertFileUpload,CompareFileUpload
 from serializers import ValidateSerializer,ConvertSerializer,CompareSerializer,ValidateSerializerReturn,ConvertSerializerReturn,CompareSerializerReturn
 from rest_framework import status
 from rest_framework.decorators import api_view,renderer_classes
 from rest_framework.renderers import BrowsableAPIRenderer,JSONRenderer
+
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 
