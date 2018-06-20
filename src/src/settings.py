adfe6e53ff3c6c869ff72c9fc4c35ff98dac94a4
gaalocastillo <gadacast@espol.edu.ec>
2018-06-20 08:30:27 -0500

    Adding PostgreSQL settings.

--- src/src/settings.py
+++ src/src/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'spdx_db',
+        'USER': 'spdx',
+        'PASSWORD': 'licensesubmittal',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
