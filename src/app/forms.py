e0fd61d53b230b96109f1dfbe0b4d83a19952522
gaalocastillo <gadacast@espol.edu.ec>
2018-07-23 06:19:24 -0500

    Updating "required" parameter on Notes, Standdard Header and Source URL license submital form field.

--- src/app/forms.py
+++ src/app/forms.py
@@ -65,9 +65,9 @@ class OrgInfoForm(forms.ModelForm):
 class LicenseRequestForm(forms.Form):
     fullname = forms.CharField(label='Fullname', max_length=70)
     shortIdentifier = forms.CharField(label='Short identifier', max_length=25)
-    sourceUrl = forms.CharField(label='Source / URL')
+    sourceUrl = forms.CharField(label='Source / URL', required=False)
     osiApproved = forms.CharField(label="OSI Approved", widget=forms.Select(choices=OSI_CHOICES))
-    notes = forms.CharField(label='Notes', )
-    licenseHeader = forms.CharField(label='Standard License Header', widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}))
+    notes = forms.CharField(label='Notes', required=False)
+    licenseHeader = forms.CharField(label='Standard License Header', widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}), required=False)
     text = forms.CharField(label='Text', widget=forms.Textarea(attrs={'rows': 3, 'cols': 40}))
     userEmail = forms.EmailField(label='Email', max_length=35)
