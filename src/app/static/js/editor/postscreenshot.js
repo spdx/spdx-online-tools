6593e315d73e51a6f7f593c645789217a2a2e181
Ugtan <umang.taneja0173@gmail.com>
2019-09-06 22:23:14 +0530

    Create an issue when license is found to be a near match not PR
    Changes took place in the commit are:
    (1) Create an issue when the license should match based on user's
    response.
    (2) Add a post screenshot script to post a SC to github.
    (3) URL of the screenshot included in issue body.

---
+++ src/app/static/js/editor/postscreenshot.js
@@ -0,0 +1,48 @@
+const repositoryNameWithOwner = "<github-username>/<repository-name>";
+const githubToken = "<github-token>";
+
+async function takeScreenshotAndUpload() {
+  let screenshot = await makeScreenshot();
+  let data = await getFileData(screenshot);
+  await postToGithub(data);
+}
+
+async function makeScreenshot(selector = ".modal-content") {
+  return new Promise((resolve, reject) => {
+    let region = document.querySelector(selector);
+
+    html2canvas(region, {
+      onrendered: canvas => {
+        let pngUrl = canvas.toDataURL();
+        resolve(pngUrl);
+      }
+    });
+  });
+}
+
+async function getFileData(imgUrl) {
+  return new Promise((resolve, reject) => {
+    resolve({
+      message: "Upload diff image",
+      content: imgUrl.split(",")[1]
+    });
+  });
+}
+
+async function postToGithub(data) {
+  let xhr = new XMLHttpRequest();
+  let filename =
+    Math.random()
+      .toString(36)
+      .substring(3) + ".png";
+
+  localStorage.setItem("filename", filename);
+  apiurl =
+    "https://api.github.com/repos/" +
+    repositoryNameWithOwner +
+    "/contents/" +
+    filename;
+  xhr.open("PUT", apiurl);
+  xhr.setRequestHeader("Authorization", "Token " + githubToken);
+  xhr.send(JSON.stringify(data));
+}
