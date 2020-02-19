50a4033a141ccdbc25733c83b4a77acd5380a5f8
anshuldutt21 <anshuldutt21@gmail.com>
2020-02-19 19:16:47 +0530

    write comments in the version file

--- src/src/version.py
+++ src/src/version.py
@@ -1,2 +1,11 @@
-spdx_version = '2.0'
-java_tools_version = '1.0.0'
+"""
+Version file for displaying the respective version of 
+the tools and online-tools repositories.
+"""
+
+spdx_version = '1.0'
+
+"""
+Visit https://github.com/spdx/tools/releases to know about the tools releases.
+"""
+java_tools_version = '2.1.20'
