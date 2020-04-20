4547526bfa979b4393358cfaf2d04b000e8729af
anshuldutt21 <anshuldutt21@gmail.com>
2020-04-20 14:20:29 +0530

    change name to github username

--- src/app/views.py
+++ src/app/views.py
@@ -1298,7 +1298,7 @@ def archiveRequests(request, license_id=None):
             github_login = user.social_auth.get(provider='github')
             token = github_login.extra_data["access_token"]
             username = github_login.extra_data["login"]
-            test = requests.get('https://api.github.com/repos/spdx/tools-python/collaborators/anshuldutt21' , headers={'Authorization': 'token {}'.format(token) })
+            test = requests.get('https://api.github.com/repos/spdx/tools-python/collaborators/{username}' , headers={'Authorization': 'token {}'.format(token) })
             if ((test.status_code == 200) or (test.status_code == 204)):
                 archive = request.POST.get('archive', True)
                 license_id = request.POST.get('license_id', False)
