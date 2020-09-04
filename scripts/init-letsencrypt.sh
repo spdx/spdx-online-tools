28ac491acd0fa4c2b949911a481a55899fb86a58
Ubuntu <ubuntu@ip-172-31-6-122.us-west-2.compute.internal>
2020-09-04 21:55:04 +0000

    Remove staging flag from letsencrypt
    Signed-off-by: Ubuntu <ubuntu@ip-172-31-6-122.us-west-2.compute.internal>

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(tools.spdx.org www.tools.spdx.org)
+domains=(tools.spdx.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="gary@sourceauditor.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
