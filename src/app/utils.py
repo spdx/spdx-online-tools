3faf524692583d3a1d4431e1ae8da16dafa920a6
Gary O'Neall <gary@sourceauditor.com>
2020-12-08 18:29:27 -0800

    Remove str.decode for license text
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/app/utils.py
+++ src/app/utils.py
@@ -506,7 +506,6 @@ def check_new_licenses_and_rejected_licenses(inputLicenseText, urlType):
 def check_spdx_license(licenseText):
     """Check the license text against the spdx license list.
     """
-    licenseText = str(licenseText.decode('string_escape'), 'utf-8')
     r = redis.StrictRedis(host=getRedisHost(), port=6379, db=0)
     
     # if redis is empty build the spdx license list in the redis database
