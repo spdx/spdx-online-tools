be8ca8914f38f19a3e29e676bbcec77ff520e2c5
Gary O'Neall <gary@sourceauditor.com>
2021-07-15 07:33:05 -0700

    Update java tools to version 1.0.2
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/src/version.py
+++ src/src/version.py
@@ -3,9 +3,9 @@ Version file for displaying the respective version of
 the tools and online-tools repositories.
 """
 
-spdx_online_tools_version = '1.0.4'
+spdx_online_tools_version = '1.0.5'
 
 """
 Visit https://github.com/spdx/tools/releases to know about the tools releases.
 """
-java_tools_version = '1.0.1'
+java_tools_version = '1.0.2'
