47c9443a3c285acd729615594d1bfe89383446d4
Gary O'Neall <gary@sourceauditor.com>
2022-04-12 11:30:04 -0700

    Update Java tool version and bump online tool version numbers (#359)
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/src/version.py
+++ src/src/version.py
@@ -3,9 +3,9 @@ Version file for displaying the respective version of
 the tools and online-tools repositories.
 """
 
-spdx_online_tools_version = '1.0.5'
+spdx_online_tools_version = '1.0.6'
 
 """
 Visit https://github.com/spdx/tools/releases to know about the tools releases.
 """
-java_tools_version = '1.0.2'
+java_tools_version = '1.0.4'
