92b5a4f56873258284b84b05aec9a929dc0fa7b0
Gary O'Neall <gary@sourceauditor.com>
2022-08-13 10:53:17 -0700

    Bump version to 1.0.7
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/src/version.py
+++ src/src/version.py
@@ -3,9 +3,9 @@ Version file for displaying the respective version of
 the tools and online-tools repositories.
 """
 
-spdx_online_tools_version = '1.0.6'
+spdx_online_tools_version = '1.0.7'
 
 """
 Visit https://github.com/spdx/tools/releases to know about the tools releases.
 """
-java_tools_version = '1.0.4'
+java_tools_version = '1.1.0'
