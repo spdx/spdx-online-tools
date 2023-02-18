63b139d7e9e11a8d969a7218884ddae07a59fa67
Hirumal Priyashan <hirupriyashanrc@gmail.com>
2023-02-19 00:45:22 +0530

    Update versions from packages themselves
    Use the Java tools and NTIA Conformance checker package themselves to obtain their version for the About page instead of hard coding.
    
    Signed-off-by: Hirumal Priyashan <hirupriyashanrc@gmail.com>

--- src/src/version.py
+++ src/src/version.py
@@ -1,12 +1,35 @@
 """
-Version file for displaying the respective version of 
+Version file for displaying the respective version of
 the tools and online-tools repositories.
 """
 
-spdx_online_tools_version = '1.1.1'
+from importlib.metadata import version
+from os.path import abspath, join, dirname
+from subprocess import run, PIPE
+from re import search
+
+
+def get_tools_version(jarName):
+    """Returns JAVA Tools version.
+
+    Arguments:
+        jarName {string} -- Name of the JAVA Tools jar file
+
+    Returns:
+        string -- JAVA Tools version
+    """
+    path = join(dirname(abspath(__file__)), "..", jarName)
+    output = run(["java", "-jar", path, "Version"], stdout=PIPE).stdout.decode("utf-8")
+    match = search("SPDX Tool Version: ([0-9]+(\.[0-9]+)+);", output)
+    if match:
+        return match.group(1)
+    return ""
+
+
+spdx_online_tools_version = "1.1.1"
 
 """
 Visit https://github.com/spdx/tools-java/releases to know about the tools releases.
 """
-java_tools_version = '1.1.5'
-ntia_conformance_checker_version = '0.2.2'
+java_tools_version = get_tools_version("tool.jar")
+ntia_conformance_checker_version = version("ntia-conformance-checker")
