f5cad48e8976ea13698c5fcbcc5603aa2d3f00b2
Gary O'Neall <gary@sourceauditor.com>
2023-02-17 17:47:37 -0800

    Update to version 1.1.1
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/src/version.py
+++ src/src/version.py
@@ -9,4 +9,4 @@ spdx_online_tools_version = '1.1.1'
 Visit https://github.com/spdx/tools-java/releases to know about the tools releases.
 """
 java_tools_version = '1.1.5'
-ntia_conformance_checker_version = '0.1.0'
+ntia_conformance_checker_version = '0.2.2'
