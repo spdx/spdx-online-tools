399060c0d27dab3e65c7a837020b71b072af83c0
BassCoder2808 <vedant.jolly@spit.ac.in>
2023-03-13 22:57:42 +0530

    Added Unit Test

--- src/app/tests.py
+++ src/app/tests.py
@@ -18,6 +18,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 import time
+import datetime
 
 from app.models import UserID
 from app.models import LicenseRequest, LicenseNamespace
@@ -1543,3 +1544,28 @@ class EditLicenseNamespaceXmlViewsTestCase(TestCase):
         self.assertEqual(resp.status_code,200)
         self.assertIn("app/license_namespace_requests.html",(i.name for i in resp.templates))
         self.assertEqual(resp.resolver_match.func.__name__,"licenseNamespaceRequests")
+
+class TestCronJob(TestCase):
+    def test_delete_old_files(self):
+        """Check if the files older than 10 days are getting deleted or not"""
+        # create a test directory with some files
+        test_dir = os.path.join(os.getcwd(), 'test_dir')
+        os.makedirs(test_dir, exist_ok=True)
+        for i in range(1, 11):
+            file_path = os.path.join(test_dir, f'test_file_{i}.txt')
+            with open(file_path, 'w') as f:
+                f.write('test')
+            # set file creation time to 11 days ago
+            creation_time = datetime.datetime.now() - datetime.timedelta(days=11)
+            if i<=5:
+                os.utime(file_path, (creation_time.timestamp(), creation_time.timestamp()))
+
+        # run the cron job to delete old files
+        # (you may need to modify this depending on how you're running the cron job)
+        os.system('python manage.py delete_old_files')
+
+        # check that only files older than 10 days were deleted
+        for i in range(1, 11):
+            file_path = os.path.join(test_dir, f'test_file_{i}.txt')
+            self.assertFalse(os.path.exists(file_path), f'{file_path} should have been deleted')
+            
