45ed163b2a49ecc563953209626f94ecae57db9b
BassCoder2808 <vedant.jolly@spit.ac.in>
2023-04-18 08:16:56 +0530

    Fixed csrf_exempt

--- src/app/views.py
+++ src/app/views.py
@@ -22,7 +22,6 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from src.version import spdx_online_tools_version
 from src.version import java_tools_version
 from src.version import ntia_conformance_checker_version
@@ -653,7 +652,6 @@ def check_license(request):
     else:
         return HttpResponseRedirect(settings.LOGIN_URL)
 
-@csrf_exempt
 def license_diff(request):
     """ View for diff section tool
     returns license_diff.html template
