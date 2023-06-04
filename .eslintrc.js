d19081f777232dbf5688466736faabf539af977a
BassCoder2808 <vedant.jolly@spit.ac.in>
2023-06-04 23:30:47 +0530

    Made changes to linters as they were failing

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,15 +9,15 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    "max-len": ["error", { code: 120 }], // Limit lines to 120 characters
-    indent: ["error", 2], // Use 2 spaces for indentation
-    "no-tabs": "error", // Disallow tabs
-    "linebreak-style": ["error", "unix"], // Enforce Unix line endings
-    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }], // Ignore unused function arguments starting with underscore (_)
+    "max-len": ["warn", { code: 120 }], // Limit lines to 120 characters
+    indent: ["warn", 2], // Use 2 spaces for indentation
+    "no-tabs": "warn", // Disallow tabs
+    "linebreak-style": ["warn", "unix"], // Enforce Unix line endings
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }], // Ignore unused function arguments starting with underscore (_)
     "no-console": "off", // Allow console statements
-    "comma-dangle": ["error", "never"], // Disallow trailing commas in arrays and objects
-    "arrow-parens": ["error", "as-needed"], // Require parentheses around arrow function parameters only when needed
-    "object-curly-spacing": ["error", "always"], // Require spaces inside curly braces of objects
-    quotes: ["error", "double", { avoidEscape: true }],
+    "comma-dangle": ["warn", "never"], // Disallow trailing commas in arrays and objects
+    "arrow-parens": ["warn", "as-needed"], // Require parentheses around arrow function parameters only when needed
+    "object-curly-spacing": ["warn", "always"], // Require spaces inside curly braces of objects
+    quotes: ["warn", "double", { avoidEscape: true }],
   },
 };
